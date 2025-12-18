23b2991cd340750ac36852eb6a01291c0094fc00
João Paulo Pinheiro <pinheiro.contatojoao@gmail.com>
2025-12-17 21:30:46 -0300

    Add Dark Mode Function

--- src/scripts/kanban.js
+++ src/scripts/kanban.js
@@ -1,13 +1,15 @@
-// Salva as tarefas adicionadas ou deletadas. No começo salva vazio.
-let tasks = JSON.parse(localStorage.getItem('KanbanTasks'))
-let dark = localStorage.getItem('DarkStatus')
+// Salva as tarefas adicionadas ou deletadas. No começo, salva vazio como um array.
+let tasks = JSON.parse(localStorage.getItem('KanbanTasks')) || []
 
-// Adiciona tarefa
+// Verifica o status do tema escuro da página e retorna um valor booleano
+let dark = localStorage.getItem('DarkStatus') === 'true'
+
+// Adiciona uma nova tarefa
 const addTask = () => {
     try {
-        const input = document.getElementById('taskInput');
+        const input = document.getElementById('taskInput')
         // Retira espaços do começo e/ou do final da string
-        const taskText = input.value.trim();
+        const taskText = input.value.trim()
 
         // Evita que o usuário adicione uma tarefa vazia
         if (taskText === '') return alert('Por favor, insira uma tarefa válida.')
@@ -21,6 +23,7 @@ const addTask = () => {
 
         // Resetar o campo de digitar a tarefa.
         input.value = ''
+        input.focus()
 
         // Salva e atualiza a lista de tarefas com a adição das novas tarefas
         saveAndRender()
@@ -30,7 +33,7 @@ const addTask = () => {
     }
 }
 
-// Mover tarefa
+// Mover uma tarefa de posição
 const moveTask = (id, direction) => {
     try {
         // Procura tarefas com id únicos (data e hora)
@@ -40,16 +43,16 @@ const moveTask = (id, direction) => {
         const order = ['todo', 'doing', 'done']
         let index = order.indexOf(task.status)
 
-        // Move para a próxima coluna se o estado da tarefa for de index < 2, ou seja, a fazer ou fazendo. Atualizando o estado atual.
+        // Move para a próxima coluna se o estado da tarefa for de index < 2, ou seja, TODO ou DOING. Atualizando o estado atual.
         if (direction === 'forward' && index < 2) { 
             task.status = order[index + 1]
 
-        // Move para a coluna anterior se o estado da tarefa for de index > , ou seja, fazendo ou feito. Atualizando o estado atual.
+        // Move para a coluna anterior se o estado da tarefa for de index > 0, ou seja, DOING ou DONE. Atualizando o estado atual.
         } else if (direction === 'back' && index > 0) {
             task.status = order[index - 1]
         }
 
-        // Salva e atualiza novamente a lista de tarefas com as alterações de posição
+        // Salva e atualiza novamente a lista de tarefas com as alterações da posição/estado da tarefa em relação ao array criado.
         saveAndRender()
         
     } catch (error) {
@@ -57,7 +60,7 @@ const moveTask = (id, direction) => {
     }
 }
 
-// Excluir tarefa
+// Excluir uma tarefa
 const deleteTask = (id) => {
     try {
         // Confirmação de exclusão
@@ -66,7 +69,7 @@ const deleteTask = (id) => {
         // Filtra todas as tarefas que não são a com o ID a ser excluído.
         tasks = tasks.filter(t => t.id !== id)
 
-        // Salva e atualiza novamente a lista de tarefas sem a tarefas que foi excluída.
+        // Salva e atualiza novamente a lista de tarefas sem a tarefa que foi excluída.
         saveAndRender()
         
     } catch (error) {
@@ -74,24 +77,30 @@ const deleteTask = (id) => {
     }
 }
 
-// Salvar e renderizar
+// Salvar e renderizar as informações do localStorage
 const saveAndRender = () => {
     localStorage.setItem('KanbanTasks', JSON.stringify(tasks))
-        
     render()
 }
 
+// Renderiza as tarefas em cada alteração que foi feita anteriormente ou que for feita depois.
 const render = () => {
 
-    document.getElementById('col-todo').innerHTML = '';
-    document.getElementById('col-doing').innerHTML = '';
-    document.getElementById('col-done').innerHTML = '';
+    // Limpa todas as colunas
+    const columns = { 
+        todo: document.getElementById('col-todo'),
+        doing: document.getElementById('col-doing'),
+        done: document.getElementById('col-done')
+    }
+
+    Object.values(columns).forEach(col => col.innerHTML = '')
 
     tasks.forEach(task => {
         const card = document.createElement('div');
-        card.className = "bg-gray-100 p-3 rounded shadow flex justify-between items-center";
+        const cardBackground = dark ? 'bg-slate-700' : 'bg-gray-100'
+        const textColor = dark ? 'text-white' : 'text-black'
+        card.className = `${cardBackground} ${textColor} p-3 rounded shadow flex justify-between items-center`
         
-        // ? : -> Mesmo que o IF e ELSE. Ou seja, caso o estado da tarefa foi X, o botão fica Y, caso contrário fica Z.
         card.innerHTML = `
             <span class="text-sm">${task.titulo}</span>
             <div class="flex gap-2">
@@ -104,43 +113,93 @@ const render = () => {
                 ? `<button onclick="moveTask(${task.id}, 'forward')" class="p-1 bg-green-500 text-white rounded hover:bg-green-600">→</button>`
                 : ""
                 }
+
                 <button onclick="deleteTask(${task.id})" class="p-1 bg-red-500 text-white rounded hover:bg-red-600">✕</button>
             </div>
             `
-        // columns[task.status].appendChild(card);
-        document.getElementById(`col-${task.status}`).appendChild(card)
+        columns[task.status].appendChild(card);
     })
+}
+
+const toggleDarkMode = (dark) => {
+
+    // Todos os elementos  que vão serem alterados na mudança de tema claro ou escuro
+    const columns = { 
+        header: document.getElementById('header'),
+        addtask: document.getElementById('addtask'),
+        colTodoContainer: document.getElementById('col-todo-container'),
+        colDoingContainer: document.getElementById('col-doing-container'),
+        colDoneContainer: document.getElementById('col-done-container'),
+        dashboardDist: document.getElementById('dashboard-dist'),
+        dashboardQtd: document.getElementById('dashboard-qtd')
+    }
+
+    const taskInput = document.getElementById('taskInput')
+    const btnDark = document.getElementById('btn_dark')
+
+    if (dark) {
+        // Se a variável dark for verdadeira, a tela, os botões e as tarefas ficam escura
+        Object.values(columns).forEach(col => {
+            col.classList.add('bg-slate-800', 'text-white')
+            col.classList.remove('bg-white')
+        })
+
+        taskInput.classList.add('bg-slate-700', 'text-white', 'border-slate-600')
+        taskInput.classList.remove('bg-white', 'text-black')
+        
+        btnDark.classList.add('bg-slate-600', 'text-white', 'hover:bg-slate-700')
+        btnDark.classList.remove('bg-gray-300', 'hover:bg-gray-400')
+          
+
+    } else {
+        // Se a variável dark for falsa, a tela fica clara
+        Object.values(columns).forEach(col => {
+            col.classList.remove('bg-slate-800', 'text-white')
+            col.classList.add('bg-white')
+        })
+
+        taskInput.classList.remove('bg-slate-700', 'text-white', 'border-slate-600')
+        taskInput.classList.add('bg-white', 'text-black')
+        
+        btnDark.classList.remove('bg-slate-600', 'text-white')
+        btnDark.classList.add('bg-gray-300', 'hover:bg-gray-400')
+    }
 }
 
 const darkMode = () => {
     dark = !dark
-    const container = document.getElementById('addtask')
+    localStorage.setItem('DarkStatus', dark)
+    toggleDarkMode(dark)
 
-    if (dark) {
-        container.classList.add('bg-stale-900 text-white')
-        container.classList.remove('bg-white')
-    } else {
-        container.classList.remove('bg-stale-900 text-white')
-        container.classList.add('bg-white')
-    }
+    render()
 }
 
+// Ao pressionar o botão Kanban, o container das tarefas aparece
 const showKanban = () => {
-    document.getElementById('kanban_section').classList.remove('hidden');
-    document.getElementById('dashboard_section').classList.add('hidden');
-    document.getElementById('btn_kanban').classList.add('bg-blue-600', 'text-white');
-    document.getElementById('btn_kanban').classList.remove('bg-white', 'text-gray-700');
-    document.getElementById('btn_dashboard').classList.add('bg-white', 'text-gray-700');
-    document.getElementById('btn_dashboard').classList.remove('bg-blue-600', 'text-white');
+    document.getElementById('container_kanban').classList.remove('hidden')
+    document.getElementById('container_dashboard').classList.add('hidden')
+    document.getElementById('btn_kanban').classList.add('bg-blue-600', 'hover:bg-blue-700')
+    document.getElementById('btn_kanban').classList.remove('bg-gray-500', 'hover:bg-gray-600')
+    document.getElementById('btn_dashboard').classList.add('bg-gray-500', 'hover:bg-gray-600')
+    document.getElementById('btn_dashboard').classList.remove('bg-blue-600', 'hover:bg-blue-700')
 }
 
+// Ao pressionar o botão Dashboard, o container dos gráficos aparece
 const showDashboard = () => {
-    document.getElementById('kanban_section').classList.add('hidden');
-    document.getElementById('dashboard_section').classList.remove('hidden');
-    document.getElementById('btn_dashboard').classList.add('bg-blue-600', 'text-white');
-    document.getElementById('btn_dashboard').classList.remove('bg-white', 'text-gray-700');
-    document.getElementById('btn_kanban').classList.add('bg-white', 'text-gray-700');
-    document.getElementById('btn_kanban').classList.remove('bg-blue-600', 'text-white');
+    document.getElementById('container_kanban').classList.add('hidden')
+    document.getElementById('container_dashboard').classList.remove('hidden')
+    document.getElementById('btn_dashboard').classList.add('bg-blue-600', 'hover:bg-blue-700')
+    document.getElementById('btn_dashboard').classList.remove('bg-gray-500', 'hover:bg-gray-600')
+    document.getElementById('btn_kanban').classList.add('bg-gray-500', 'hover:bg-gray-600')
+    document.getElementById('btn_kanban').classList.remove('bg-blue-600', 'hover:bg-blue-700')
 }
 
-render()
+// Ao inicializar a página, renderiza as tarefas.
+window.onload = () => {
+    render()
+
+    // Chama a função de tema dark ao recarregar a página, apenas se ela já estava ativada antes.
+    if (dark) {
+        toggleDarkMode(true)
+    } 
+}
